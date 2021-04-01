71544c2a64043ba0f70501717a05a7ff56a2de29
luissimao <luissimao@ipvc.pt>
2021-04-01 15:48:30 +0100

    todos os layouts verticais terminados, começar a tratar dos webservices

---
+++ app/src/main/java/ipvc/estg/afproject/AddOccurrence.kt
@@ -0,0 +1,28 @@
+package ipvc.estg.afproject
+
+import android.R.attr.data
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AddOccurrence : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_occurrence)
+    }
+
+    fun back(view: View) {
+        val intent = Intent(this, MapScreen::class.java)
+        startActivity(intent)
+    }
+
+    fun submit(view: View) {
+        val toast = Toast.makeText(applicationContext, "Hello", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+}
--- app/src/main/java/ipvc/estg/afproject/MapScreen.kt
+++ app/src/main/java/ipvc/estg/afproject/MapScreen.kt
@@ -4,27 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MapScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_map_screen)
     }
 
-    fun button4(view: View) {
+    fun addOccurrence(view: View) {
 
-        val intent = Intent(this, MainActivity2::class.java)
+        val intent = Intent(this, AddOccurrence::class.java)
 
         startActivity(intent)
     }
 
-    fun buttonMapa(view: View) {
+    fun logout(view: View) {
 
-        val intent = Intent(this, MapScreen::class.java)
+        val intent = Intent(this, MainActivity::class.java)
 
         startActivity(intent)
     }
-
 
 }
